b37fd836dd6899bd59d5807e304a2411d267bd18
Alessandro dos Santos <professor_alessandro@hotmail.com>
2021-05-15 12:15:55 -0300

    Versao funcional antes de implementar o PDF

--- POCCropWindowsForms/Form1.Designer.cs
+++ POCCropWindowsForms/Form1.Designer.cs
@@ -35,6 +35,12 @@ namespace POCCropWindowsForms
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.BtnSelecionarArquivo = new System.Windows.Forms.Button();
+            this.textPathArquivo = new System.Windows.Forms.TextBox();
+            this.textPathArquivoSaida = new System.Windows.Forms.TextBox();
+            this.btnDiretorioSaida = new System.Windows.Forms.Button();
+            this.BtnSaveImageToPDF = new System.Windows.Forms.Button();
+            this.btnSelectCroppArea = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -43,15 +49,15 @@ namespace POCCropWindowsForms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(392, 74);
+            this.label1.Location = new System.Drawing.Point(307, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 36);
+            this.label1.Size = new System.Drawing.Size(558, 36);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Altran crop tool in C#";
+            this.label1.Text = "Alessandro Programing - Image crop tool";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(121, 165);
+            this.pictureBox1.Location = new System.Drawing.Point(121, 164);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(300, 247);
             this.pictureBox1.TabIndex = 1;
@@ -67,19 +73,19 @@ namespace POCCropWindowsForms
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(346, 432);
+            this.button1.Location = new System.Drawing.Point(121, 511);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(300, 34);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Select area";
+            this.button1.Text = "Selecionar area de recorte";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(715, 432);
+            this.button2.Location = new System.Drawing.Point(715, 417);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(300, 34);
             this.button2.TabIndex = 4;
             this.button2.Text = "Crop area";
             this.button2.UseVisualStyleBackColor = true;
@@ -89,17 +95,76 @@ namespace POCCropWindowsForms
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(826, 421);
+            this.label2.Location = new System.Drawing.Point(709, 457);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(178, 36);
             this.label2.TabIndex = 5;
             this.label2.Text = "Dimensions:";
+            // 
+            // BtnSelecionarArquivo
+            // 
+            this.BtnSelecionarArquivo.Location = new System.Drawing.Point(313, 457);
+            this.BtnSelecionarArquivo.Name = "BtnSelecionarArquivo";
+            this.BtnSelecionarArquivo.Size = new System.Drawing.Size(108, 20);
+            this.BtnSelecionarArquivo.TabIndex = 6;
+            this.BtnSelecionarArquivo.Text = "Selecionar Arquivo";
+            this.BtnSelecionarArquivo.UseVisualStyleBackColor = true;
+            this.BtnSelecionarArquivo.Click += new System.EventHandler(this.BtnSelecionarArquivo_Click);
+            // 
+            // textPathArquivo
+            // 
+            this.textPathArquivo.Location = new System.Drawing.Point(121, 457);
+            this.textPathArquivo.Name = "textPathArquivo";
+            this.textPathArquivo.Size = new System.Drawing.Size(186, 20);
+            this.textPathArquivo.TabIndex = 7;
+            // 
+            // textPathArquivoSaida
+            // 
+            this.textPathArquivoSaida.Location = new System.Drawing.Point(121, 485);
+            this.textPathArquivoSaida.Name = "textPathArquivoSaida";
+            this.textPathArquivoSaida.Size = new System.Drawing.Size(186, 20);
+            this.textPathArquivoSaida.TabIndex = 9;
+            // 
+            // btnDiretorioSaida
+            // 
+            this.btnDiretorioSaida.Location = new System.Drawing.Point(313, 485);
+            this.btnDiretorioSaida.Name = "btnDiretorioSaida";
+            this.btnDiretorioSaida.Size = new System.Drawing.Size(108, 20);
+            this.btnDiretorioSaida.TabIndex = 8;
+            this.btnDiretorioSaida.Text = "Diretório de saída";
+            this.btnDiretorioSaida.UseVisualStyleBackColor = true;
+            this.btnDiretorioSaida.Click += new System.EventHandler(this.btnDiretorioSaida_Click);
+            // 
+            // BtnSaveImageToPDF
+            // 
+            this.BtnSaveImageToPDF.Location = new System.Drawing.Point(121, 551);
+            this.BtnSaveImageToPDF.Name = "BtnSaveImageToPDF";
+            this.BtnSaveImageToPDF.Size = new System.Drawing.Size(300, 34);
+            this.BtnSaveImageToPDF.TabIndex = 10;
+            this.BtnSaveImageToPDF.Text = "Salvar imagem como PDF";
+            this.BtnSaveImageToPDF.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectCroppArea
+            // 
+            this.btnSelectCroppArea.Location = new System.Drawing.Point(121, 417);
+            this.btnSelectCroppArea.Name = "btnSelectCroppArea";
+            this.btnSelectCroppArea.Size = new System.Drawing.Size(300, 34);
+            this.btnSelectCroppArea.TabIndex = 11;
+            this.btnSelectCroppArea.Text = "Recortar Area Selecionada";
+            this.btnSelectCroppArea.UseVisualStyleBackColor = true;
+            this.btnSelectCroppArea.Click += new System.EventHandler(this.btnSelectCroppArea_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1211, 607);
+            this.Controls.Add(this.btnSelectCroppArea);
+            this.Controls.Add(this.BtnSaveImageToPDF);
+            this.Controls.Add(this.textPathArquivoSaida);
+            this.Controls.Add(this.btnDiretorioSaida);
+            this.Controls.Add(this.textPathArquivo);
+            this.Controls.Add(this.BtnSelecionarArquivo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -124,6 +189,12 @@ namespace POCCropWindowsForms
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnSelecionarArquivo;
+        private System.Windows.Forms.TextBox textPathArquivo;
+        private System.Windows.Forms.TextBox textPathArquivoSaida;
+        private System.Windows.Forms.Button btnDiretorioSaida;
+        private System.Windows.Forms.Button BtnSaveImageToPDF;
+        private System.Windows.Forms.Button btnSelectCroppArea;
     }
 }
 
--- POCCropWindowsForms/Form1.cs
+++ POCCropWindowsForms/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,15 @@ namespace POCCropWindowsForms
 {
     public partial class Form1 : Form
     {
+        public static string path = @"C:\Users\profe\Pictures\AP - Imagens\";
+        public static string mainFile = "170013218_1691096707739234_6392655146255409480_n";
+        public static string mainFileType = ".jpg";
+        public static string pathmainFileFull = path + mainFile + mainFileType;
+
+        public static string pathOut = @"C:\Users\profe\Pictures\AP - Imagens\Imagens de saida\";
+        public static string mainFileOut = "170013218_1691096707739234_6392655146255409480_n";
+        public static string mainFileTypeOut = ".jpg";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +31,36 @@ namespace POCCropWindowsForms
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Load image
-            Bitmap bmp = new Bitmap(@"C:\Users\profe\source\repos\Alessandro-Programming\Repo solutions for image\POCCropWindowsForms\images\170013218_1691096707739234_6392655146255409480_n.jpg");
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.Image = (Image)bmp;
+            // PREPARA OS CONTROLES
+            
+            this.textPathArquivo.Enabled = false;
+            this.textPathArquivoSaida.Enabled = false;
+
+            // VERIFICA O DIRETORIO DO PROGRAMA SE ELE EXISTE E SE NAO EXISTIR ELE E CRIADO
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            // AQUI VERIFICA SE O ARQUIVO RAIZ EXISTE PARA EXIBIR NO IMAGEDIALOG
+            if (File.Exists(pathmainFileFull))
+            {
+                //Load image
+                this.SetImageOnFrame(pathmainFileFull);
+                this.textPathArquivo.Text = pathmainFileFull;
+            }
+            else
+            {
+                this.textPathArquivo.Text = path;
+            }
+
+            // TRATAMENTO DIRETORIO DE SAIDA
+            if (!Directory.Exists(pathOut))
+            {
+                Directory.CreateDirectory(pathOut);
+            }
+
+            this.textPathArquivoSaida.Text = pathOut;
         }
 
         //declare some variable for crop coordinates
@@ -81,10 +119,87 @@ namespace POCCropWindowsForms
             pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
         }
 
+        private void btnDiretorioSaida_Click(object sender, EventArgs e)
+        {
+            
+
+            using (var openDlg = new FolderBrowserDialog())
+            {
+                openDlg.SelectedPath = pathOut;
+
+                if (openDlg.ShowDialog() == DialogResult.OK)
+                {
+                    // CAMINHO DA PASTA SELECIONADA
+                    this.textPathArquivoSaida.Text = "";
+                    pathOut = openDlg.SelectedPath;
+                    this.textPathArquivoSaida.Text = pathOut;
+                }
+            }
+        }
+
+        private void BtnSelecionarArquivo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var openDlg = new OpenFileDialog())
+                {
+                    openDlg.InitialDirectory = path;
+                    openDlg.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+                    openDlg.CheckFileExists = true;
+                    openDlg.CheckPathExists = true;
+                    openDlg.FilterIndex = 2;
+                    openDlg.RestoreDirectory = true;
+                    openDlg.ReadOnlyChecked = true;
+                    openDlg.ShowReadOnly = true;
+                    openDlg.Multiselect = false;
+
+                    if (openDlg.ShowDialog() == DialogResult.OK)
+                    {
+                        // CAMINHO DA PASTA SELECIONADA
+                        this.textPathArquivo.Text = "";
+                        pathmainFileFull = openDlg.FileName;
+                        //Load image
+                        this.SetImageOnFrame(pathmainFileFull);
+                        this.textPathArquivo.Text = pathmainFileFull;
+                        // path = openDlg.;
+                        // this.textPathArquivo.Text = path;
+                    }
+                }
+            }
+            catch (SecurityException ex)
+            {
+                // O usuário  não possui permissão para ler arquivos
+                MessageBox.Show("Erro de segurança Contate o administrador de segurança da rede.\n\n" +
+                                            "Mensagem : " + ex.Message + "\n\n" +
+                                            "Detalhes (enviar ao suporte):\n\n" + ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                // Não pode carregar a imagem (problemas de permissão)
+                MessageBox.Show("Não é possível exibir a imagem : " + pathmainFileFull
+                                           + ". Você pode não ter permissão para ler o arquivo , ou " +
+                                           " ele pode estar corrompido.\n\nErro reportado : " + ex.Message);
+            }
+
+
+        }
+
         private void pictureBox1_MouseEnter(object sender, EventArgs e)
         {
             base.OnMouseEnter(e);
             Cursor = Cursors.Cross;
+        }
+
+        private void btnSelectCroppArea_Click(object sender, EventArgs e)
+        {
+            //here we declare mouse event handlers
+
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+
+            pictureBox1.MouseEnter += new EventHandler(pictureBox1_MouseEnter);
+            Controls.Add(pictureBox1);
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -101,13 +216,50 @@ namespace POCCropWindowsForms
                 g.Dispose();
             }
         }
+
         protected override void OnMouseEnter(EventArgs e)
         {
             base.OnMouseEnter(e);
             Cursor = Cursors.Default;
         }
 
+        // METODOS DA CLASSE
+        private void SetImageOnFrame(string file)
+        {
+            Bitmap bmp = new Bitmap(file);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = (Image)bmp;
+        }
 
+        protected void GerarPDF()
+        {
+            //Definindo tamanho do documento
+            Document document = new Document(new iTextSharp.text.Rectangle(3508, 2480),40,40,40,40);
+            
+            MemoryStream msDoc = new MemoryStream();
+            
+            //Criando documento
+            PdfWriter writer = PdfWriter.GetInstance(document, msDoc);
+            document.Open();
 
+            //Pegando localização da imagem
+            string localizacao = Server.MapPath("imagem.jpg");
+            
+            //Definindo resolução
+            iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(localizacao);
+            img.ScaleToFit(3428, 2400);
+            
+            IElement e = img;          
+            doc.Add(e);         
+            doc.Close();
+            
+            //Mandando a imagem para o navegador
+            Response.Clear();
+            Response.AddHeader("content-disposition", "attachment;filename=Arquivo.pdf");
+            Response.ContentType = "application/pdf";
+            Response.BinaryWrite(msDoc.ToArray());
+            Response.End(); 
+            
+        }
     }
 }
